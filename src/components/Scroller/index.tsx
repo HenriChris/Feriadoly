c2bdcc88e276c5750a175b34abf7ef4ea1cffacb
unset <henrique.chrispim@protonmail.com>
2023-03-10 15:24:28 -0300

    Criado o código do Scroller e sua estilização

--- src/components/Scroller/index.tsx
+++ src/components/Scroller/index.tsx
@@ -1,5 +1,136 @@
-function Scroller() {
-    return <div></div>
+import React, { useEffect, useState } from 'react';
+import { Holidays, HolidaysDate, HolidaysText, ScrollerWrapper, StyledScroller, Title } from './style';
+
+type datetime = {
+    day: number,
+    month: number,
+    year: number
+}
+
+type date = {
+    datetime: datetime
+}
+
+type scrollerInfo = {
+    name: string,
+    date: date
+}
+
+function ScrollerElement ({
+    name,
+    date
+}: scrollerInfo) {
+    return (
+        <Holidays>
+            <HolidaysDate>{date.datetime.day}/{date.datetime.month}/{date.datetime.year}</HolidaysDate>
+            <HolidaysText>{name}</HolidaysText>
+        </Holidays>
+    )
+}
+
+function Scroller () {
+    const [holidays, setHolidays] = useState<scrollerInfo[]>();
+ 
+    const feriados : scrollerInfo[] = [
+        {
+            name: 'New Years Day',
+            date: {
+                datetime : {
+                    day: 10,
+                    month: 1,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Carnival Monday',
+            date: {
+                datetime : {
+                    day: 20,
+                    month: 2,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Carnival Tuesday',
+            date: {
+                datetime : {
+                    day: 21,
+                    month: 2,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Carnival End',
+            date: {
+                datetime : {
+                    day: 22,
+                    month: 2,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Good Friday',
+            date: {
+                datetime : {
+                    day: 7,
+                    month: 4,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Tiradentes Day',
+            date: {
+                datetime : {
+                    day: 21,
+                    month: 4,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Labor Day',
+            date: {
+                datetime : {
+                    day: 1,
+                    month: 5,
+                    year: 2023,
+                }
+            }
+        },
+        {
+            name: 'Corpus Christi',
+            date: {
+                datetime : {
+                    day: 8,
+                    month: 6,
+                    year: 2023,
+                }
+            }
+        }
+    ]
+    useEffect(() => {setHolidays(feriados)}, [])
+
+    return (
+        <ScrollerWrapper>
+        <Title>Feriados em 2023 :</Title>
+        <StyledScroller> {/* shadow */}
+            {
+            holidays?.map((holiday) =>
+                <ScrollerElement
+                    key={holiday.name}
+                    date={holiday.date}
+                    name={holiday.name}
+                />
+                )
+            }
+        </StyledScroller>
+        </ScrollerWrapper>
+    )
 }
 
 export default Scroller;
--- src/components/Scroller/style.tsx
+++ src/components/Scroller/style.tsx
@@ -1,2 +1,54 @@
 import styled from 'styled-components';
 import { style } from '../../global/global';
+
+export const ScrollerWrapper = styled.div`
+    margin: 12px auto 35px auto;
+    max-width: 1000px;
+    font-size: 2rem;
+    color : ${style.colors.brownishRed};
+`;
+
+export const Title = styled.span`
+    margin-left: 10px;
+`;
+
+export const StyledScroller = styled.div`
+    height : 327px;
+
+    background: ${style.colors.lightBeige};
+
+    margin-top: 12px;
+    border : 6px solid ${style.colors.pink};
+    border-radius: 15px;
+
+    overflow-y: scroll;
+`;
+
+export const Holidays = styled.div`
+    display: flex;
+    height: 81px;
+    
+    font-weight: 600;
+    font-size: 32px;
+
+    border-bottom: 1px solid ${style.colors.brownishRed};
+
+    overflow-x: hidden;
+    white-space: nowrap;
+`;
+
+export const HolidaysDate = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 272px;
+
+    border-right: 1px solid ${style.colors.brownishRed};
+`;
+
+export const HolidaysText = styled.div`
+    display: flex;
+    align-items: center;
+    
+    padding-left: 40px;
+`;
