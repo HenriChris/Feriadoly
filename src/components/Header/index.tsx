09a042f0fbba79e47837c88cccba765de4142ab3
unset <henrique.chrispim@protonmail.com>
2023-03-10 14:49:18 -0300

    Criado o código do Header e sua estilização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,10 @@
-function Header() {
-    return <div></div>
+import React, { useState } from 'react';
+import { StyledHeader } from './style';
+
+function Header () {
+    return (
+        <StyledHeader>Feriadoly</StyledHeader>
+    )
 }
 
 export default Header;
--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -1,2 +1,14 @@
 import styled from 'styled-components';
 import { style } from '../../global/global';
+
+export const StyledHeader = styled.div`
+    display: flex;
+    justify-content: center;
+    
+    font-size: 128px;
+
+    background-color: ${style.colors.red};
+    color: ${style.colors.white};
+
+    user-select: none;
+`;
