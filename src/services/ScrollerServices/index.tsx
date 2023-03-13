ef9d3013ae35e0607a5ef8c562bc9c58aca7cccc
unset <henrique.chrispim@protonmail.com>
2023-03-13 19:54:58 -0300

    feature: calendar e, consequentemente, card agoram recebem dados da api externa

---
+++ src/services/ScrollerServices/index.tsx
@@ -0,0 +1,13 @@
+import api from "../api";
+/* apikey = c830eba06186831261fab92aa4b6325727b35a8e */
+export default {
+    async getHolidaysScroller(country : string, year : string, day : string, month : string, apiKey : string) {
+
+        try {
+                const response = api.get('v2/holidays?api_key=' + apiKey + '&country=' + country + '&year=' + year + '&day=' + day + '&month=' + month);
+            return response;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
