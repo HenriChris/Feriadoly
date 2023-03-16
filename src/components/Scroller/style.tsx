892eef2316c89e40a95dd777ed87122c4f466d35
unset <henrique.chrispim@protonmail.com>
2023-03-15 21:48:45 -0300

    bugfix: agora o scroller é mais responsivo

--- src/components/Scroller/style.tsx
+++ src/components/Scroller/style.tsx
@@ -2,18 +2,24 @@ import styled from 'styled-components';
 import { Shadow, style } from '../../global/global';
 
 export const ScrollerWrapper = styled.div`
-    margin: 12px auto 35px auto;
-    width: 1000px;
-    font-size: 32px;
+    margin: 2% auto 2% auto;
+    width: 95%;
+    max-width: 1000px;
+    /* font-size: 32px; */
+    font-size: 2rem;
+    @media (max-width: 480px) {
+        font-size: 1.2rem;
+    }
+    /* font-size: 3.6vh; */
     color : ${style.colors.brownishRed};
 `;
 
 export const ScrollerTitle = styled.span`
-    margin-left: 10px;
+    margin-left: 1%;
 `;
 
 export const StyledScroller = styled.div`
-    height : 327px;
+    height : 40vh;
     background: ${style.colors.lightBeige};
     border-radius: 15px;
     overflow-y: scroll;
@@ -37,15 +43,15 @@ export const StyledScroller = styled.div`
 
 export const ScrollerContent = styled(Shadow)`
     background: ${style.colors.lightBeige};
-    padding-right: 10px;
+    padding-right: 1%;
     border-radius: 15px;
 `;
 
 export const Holidays = styled.div`
     display: flex;
-    height: 81px;
+    /* height: 24.7325%; */
+    height: 10vh;
     font-weight: 600;
-    font-size: 32px;
     border-bottom: 1px solid ${style.colors.brownishRed};
     overflow-x: hidden;
     white-space: nowrap;
@@ -55,7 +61,9 @@ export const HolidaysDate = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 272px;
+    /* width: 272px; */
+    width: 28.0412371%;
+    min-width: 9.5rem;
 
     border-right: 1px solid ${style.colors.brownishRed};
 `;
@@ -63,6 +71,5 @@ export const HolidaysDate = styled.div`
 export const HolidaysText = styled.div`
     display: flex;
     align-items: center;
-    
-    padding-left: 40px;
+    margin-left: 3vw;
 `;
