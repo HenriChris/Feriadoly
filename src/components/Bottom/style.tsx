384570adffba8cf6f7576403d0054964d30d376e
unset <henrique.chrispim@protonmail.com>
2023-03-15 22:41:53 -0300

    bugfix: agora a seção inferior do app é mais responsiva

--- src/components/Bottom/style.tsx
+++ src/components/Bottom/style.tsx
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 export const DateContextProviderWrapper = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     margin-left: auto;
     margin-right: auto;
-    width: 1000px;
+    width: 95%;
+    max-width: 1000px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
